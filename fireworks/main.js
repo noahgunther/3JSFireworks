67014feeab7d84dc3a136ab6db4bfbd56d9d7b50
Noah Gunther <noah.gunther@gmail.com>
2023-02-04 23:05:30 -0500

    Tweaking night sky

--- fireworks/main.js
+++ fireworks/main.js
@@ -63,11 +63,11 @@ function init() {
   scene.background = new THREE.Color(0x000005);
 
   // Night sky
-  const starCount = 300;
+  const starCount = 400;
   var stars = [];
   const starMaterial = new THREE.MeshBasicMaterial({ color : new THREE.Color(1.0, 0.7, 0.5) });
   for (let i = 0; i < starCount; i++) {
-    const starGeometry = new THREE.SphereGeometry(0.01 * Math.max(Math.random(), 0.5), 3.0, 3.0);
+    const starGeometry = new THREE.SphereGeometry(0.0175 * Math.max(Math.random(), 0.5), 3.0, 3.0);
     const starMesh = new THREE.Mesh(starGeometry, starMaterial);
     stars.push(starMesh);
     starMesh.position.set(Math.random() * 26.0 - 13.0, Math.random() * 20.0 - 10.0, (Math.random() * 2.0 - 1.0) - 40);
